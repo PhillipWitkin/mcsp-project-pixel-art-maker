2d46ae1595c63b4b150eb53ed81418dbc266eab1
Phillip Witkin <pwitkin273@gmail.com>
2022-11-11 00:51:27 -0500

    can color by dragging

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ canvas.style.width = `${PIXEL_WIDTH * NUM_PIXELS}px`;
 function createGrid(){
   for(let i = 1; i <= NUM_PIXELS ** 2; i++) {
     const pixel = document.createElement('button');
-    pixel.className = "pixel";
+    pixel.className = 'pixel';
     pixel.style.width = `${PIXEL_WIDTH}px`;
     pixel.style.height = `${PIXEL_WIDTH}px`;
-    pixel.setAttribute("pixel-number", i)
-    // pixel.addEventListener('click', () => {
-    //   pixel.style.background = 'red';
-    // });
+    pixel.setAttribute('pixel-number', i)
     canvas.append(pixel);
   }
 }
@@ -39,42 +36,45 @@ function createPalette(){
   return [redPicker, greenPicker, bluePicker]
 }
 
+// This will be called by the event listener, automatically has access to triggering 'event'
 function colorPixelCallback(pixel, color){
-  return function(){
-    console.log("Pixel " + pixel.getAttribute("pixel-number") + " is being colored: " + color);
-    pixel.style.backgroundColor = color;
+  return function(event){
+    console.log("Event: " + event.type, "on Grid Button: " + event.target.getAttribute("pixel-number"));
+    const pixNum = pixel.getAttribute('pixel-number');
+    if (event.type == 'click'){
+      console.log("Pixel: " + pixNum, " is being colored by click: " + color);
+      pixel.style.backgroundColor = color;
+    } else if (event.type == 'mouseenter' && mouseState == 'down'){
+      console.log("Pixel: " + pixNum, " is being colored by drag: " + color);
+      pixel.style.backgroundColor = color;
+    } else if (event.type == 'mousedown'){
+      console.log("Pixel: " + pixNum, " is being colored... " + color);
+      mouseState = 'down';
+      pixel.style.backgroundColor = color;
+    }
+
   }
 }
-
-// function colorPixel(pixel, color){
-//   console.log("Pixel " + pixel.getAttribute("pixel-number") + " is being colored: " + color);
-//   pixel.style.backgroundColor = color;
-// }
-
-// function setupPixelListeners(color){
-//   pixels = document.querySelectorAll("button");
-//   for (const pixel of pixels){
-//     // Add event listener as anonymous func 
-//     pixel.addEventListener("click",
-//     function(event){
-//       console.log("Pixel clicked..." + pixel.getAttribute("pixel-number"));
-//       colorPixel(event.target, color);
-//     });
-//   }
-// }
 
 function setupPixelListeners(color){
-  pixels = document.querySelectorAll("button");
+  pixels = document.querySelectorAll('.pixel');
   for (const pixel of pixels){    
-    pixel.addEventListener("click", colorPixelCallback(pixel, color));
+    pixel.addEventListener('click', colorPixelCallback(pixel, color));
+    pixel.addEventListener('mouseenter', colorPixelCallback(pixel, color));
+    // attach listeners for mouse up and down to color grid by dragging
+    pixel.addEventListener('mousedown', colorPixelCallback(pixel, color));
+    pixel.addEventListener('mouseup', () => mouseState = 'up');
   }
 }
+
+
+var mouseState = 'up';
 
 createGrid()
 colorOptions = createPalette()
 for (const color of colorOptions) {
-  color.addEventListener("click", () => {
-    colorString = color.style.backgroundColor;
+  color.addEventListener('click', () => {
+    const colorString = color.style.backgroundColor;
     console.log("Selected color: " + colorString);
     setupPixelListeners(colorString)
   });
